d6c861834a8d642fe8e424e246b7413916b29ccd
James Luttringer <jamesluttringer18@gmail.com>
2020-03-05 16:01:42 -0500

    ?

--- api/app.py
+++ api/app.py
@@ -7,6 +7,9 @@ from flask import Flask, request, jsonify
 from spotipy.oauth2 import SpotifyClientCredentials
 
 def create_app():
+    '''
+    Create the app, load the model and lookup table, and initiate the spotify client
+    '''
     app = Flask(__name__)
     client_credentials_manager = SpotifyClientCredentials(client_id='69d7960353114a25ad479492dd0346eb', client_secret='6190f3b487b448d6b0d8340c01baf3ab')
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
@@ -29,7 +32,10 @@ def create_app():
 
     @app.route('/song', methods=['GET'])
     def song_info():
-        '''This route returns details about the specified song'''
+        '''
+        This route formats user input, uses it to find the song in the 
+        spotify api, then returns information about it as a json object
+        '''
         title = request.args.get('title')
         artist = request.args.get('artist')
 
@@ -42,22 +48,33 @@ def create_app():
 
     @app.route('/suggestions', methods=['GET'])
     def suggest():
-
+        '''
+        This endpoint take a user inputted song/artist, retrieves the audio features for 
+        the input song from the spotify api, preprocesses the data input, and retrieves the
+        predictions from our nearest neighbors model.
+        '''
+        # Use the input to retrieve the song id from spotify
         title = request.args.get('title')
         artist = request.args.get('artist')
         song = sp.search(f'{title} {artist}', type='track', limit=1)
         try:
             song_id = song['tracks']['items'][0]['id']
         except:
+            # Return error message if the song is not on spotify
             return("Sorry, we couldn't find the song you're looking for.")
         else:
+            # Query the input song's audio features 
             song_features = sp.audio_features(song_id)[0]
             x = np.array([song_features[feature] for feature in features]).reshape(1,-1)
+            # Preprocess the input data
             x[0][2] = x[0][2]/11
             x[0][3] = x[0][3]/58.882
             x[0][10] = x[0][10]/249.983
+            # Get the predictions from the model
             ids = nn.kneighbors(x)[1][0]
+            # Map predictions to their spotify id
             ids = [id_map[id] for id in ids]
+            # Retrieve the song information and audio features, return them as a json object
             s = sp.tracks(ids)
             s = [s['tracks'][ind] for ind in range(len(s['tracks']))]
             s = [{'title': t['name'],
@@ -71,6 +88,11 @@ def create_app():
 
     @app.route('/least', methods=['GET'])
     def least():
+        '''
+        This endpoint takes user input, and processes it in order
+        to return the least similar song from out model
+        '''
+        # Retrieve the song features
         title = request.args.get('title')
         artist = request.args.get('artist')
         song = sp.search(f'{title} {artist}', type='track', limit=1)
@@ -81,10 +103,13 @@ def create_app():
         else:
             song_features = sp.audio_features(song_id)[0]
             x = np.array([song_features[feature] for feature in features]).reshape(1,-1)
+            # Preprocess, notice everything here is the same, except the values are subtracted from 1
+            # this is how we get the model to return the least similar song
             x[0][2] = x[0][2]/11
             x[0][3] = x[0][3]/58.882
             x[0][10] = x[0][10]/249.983
             x = 1 - x
+            #return the information of the least similar song
             out = nn.kneighbors(x)[1][0][0]
             out = id_map[out]
             s = sp.track(out)
